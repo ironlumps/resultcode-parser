3193d0ad2e8034f5945bddd04d7c50cccae31b72
Ryan Kassab <33270781+ironlumps@users.noreply.github.com>
2018-08-17 12:43:44 -0700

    Add files via upload

---
+++ ResultCodeDescriptions/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ResultCodeDescriptions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkShort = new System.Windows.Forms.CheckBox();
+            this.checkLong = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(29, 159);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(120, 44);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(215, 159);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(120, 44);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkShort
+            // 
+            this.checkShort.AutoSize = true;
+            this.checkShort.Checked = true;
+            this.checkShort.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkShort.Location = new System.Drawing.Point(29, 85);
+            this.checkShort.Name = "checkShort";
+            this.checkShort.Size = new System.Drawing.Size(107, 17);
+            this.checkShort.TabIndex = 4;
+            this.checkShort.Text = "Short Description";
+            this.checkShort.UseVisualStyleBackColor = true;
+            // 
+            // checkLong
+            // 
+            this.checkLong.AutoSize = true;
+            this.checkLong.Checked = true;
+            this.checkLong.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkLong.Location = new System.Drawing.Point(29, 118);
+            this.checkLong.Name = "checkLong";
+            this.checkLong.Size = new System.Drawing.Size(106, 17);
+            this.checkLong.TabIndex = 5;
+            this.checkLong.Text = "Long Description";
+            this.checkLong.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(393, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(270, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 257);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkLong);
+            this.Controls.Add(this.checkShort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Result Code Parse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkShort;
+        private System.Windows.Forms.CheckBox checkLong;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ResultCodeDescriptions/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using HtmlAgilityPack;
+
+
+namespace ResultCodeDescriptions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.InitialDirectory = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            fdlg.Filter = "All files(*.*)| *.*| HTML(*.html) | *.csv | TXT(*.txt) | *.txt";
+            fdlg.FilterIndex = 3;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = fdlg.FileName;
+                StreamReader parse = new StreamReader(fileName);
+                string writing = fileName.Replace(".txt", "Results.txt");
+                StreamWriter write = new StreamWriter(writing);
+
+                string result = "";
+
+                string value = parse.ReadToEnd();
+
+                while (value != null)
+                {
+                    string[] split = value.Split('$');
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        string descrip = split[i];
+                        string[] descripSplit = descrip.Split('|');
+                        write.WriteLine(descripSplit[1] + "," + descripSplit[2] + "," + descripSplit[3]);
+                    }
+                }
+
+                StreamReader reader = new StreamReader(writing);
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                }
+
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            Parse();
+
+            Application.Exit();
+        }
+
+        //public string ResultCode(string code, bool checkShort, bool checkLong)
+        //{
+        //    string shor = "";
+        //    string lon = "";
+
+        //    if(code.Equals("ASO1"))
+        //    {
+        //        shor = "Address Fully Verified";
+        //        lon = "The address is valid and deliverable according to official postal agencies.";
+        //    }
+        //    else if(code.Equals("AS02"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS03"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS09"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS10"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS11"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS12"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS13"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS14"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS15"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS16"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS17"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS18"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS20"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS22"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS23"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //    else if (code.Equals("AS24"))
+        //    {
+        //        shor = "Street Only Match";
+        //        lon = "The street address was verified but the suite/apartment number is missing or invalid.";
+        //    }
+        //}
+        public void Parse()
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.InitialDirectory = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            fdlg.Title = "Choose Result Codes - Melissa Data WikiResults.txt";
+            fdlg.Filter = "All files(*.*)| *.*| HTML(*.html) | *.csv | TXT(*.txt) | *.txt";
+            fdlg.FilterIndex = 3;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = fdlg.FileName;
+                string writing = fileName.Replace(".txt", "Results.txt");
+                if (!File.Exists(writing))
+                {
+                    StreamReader parse = new StreamReader(fileName);
+
+                    StreamWriter write = new StreamWriter(writing);
+
+                    string result = "";
+
+                    string value = parse.ReadToEnd();
+
+
+                    string[] split = value.Split('@');
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        if (!split[i].Contains("<th>"))
+                        {
+                            string descrip = split[i];
+                            descrip = descrip.Replace('\n', ' ');
+                            string[] descripSplit = descrip.Split('|');
+                            if (descripSplit.Length == 1)
+                            {
+                                write.WriteLine(descripSplit[0]);
+                            }
+                            else if (descripSplit.Length == 2)
+                            {
+                                write.WriteLine(descripSplit[0] + "," + descripSplit[1]);
+                            }
+                            else
+                            {
+                                write.WriteLine(descripSplit[0] + "," + descripSplit[1] + "," + descripSplit[2]);
+                            }
+
+                        }
+
+                    }
+
+                    write.Close();
+                    write.Dispose();
+                    parse.Close();
+                }
+                else
+                {
+                    OpenFileDialog secondDialog = new OpenFileDialog();
+                    secondDialog.InitialDirectory = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+                    secondDialog.Filter = "All files(*.*)| *.*| HTML(*.html) | *.csv | TXT(*.txt) | *.txt";
+                    secondDialog.Title = "Choose the file you want to run through";
+                    secondDialog.FilterIndex = 3;
+                    secondDialog.RestoreDirectory = true;
+                    if (secondDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string newFile = secondDialog.FileName;
+                        StreamReader goal = new StreamReader(newFile);
+                        if (newFile.EndsWith(".csv"))
+                        {
+                            newFile = newFile.Replace(".csv", "FinalResults.csv");
+                        }
+                        if (newFile.EndsWith(".txt"))
+                        {
+                            newFile = newFile.Replace(".txt", "FinalResults.txt");
+                        }
+
+
+
+                        string results = "";
+                        string goalLine;
+                        while ((goalLine = goal.ReadLine()) != null)
+                        {
+                            string[] goalSplit = goalLine.Split(',');
+
+                            for (int i = 0; i < goalSplit.Length; i++)
+                            {
+                                StreamReader basic = new StreamReader(writing);
+                                StreamWriter finished = new StreamWriter(newFile, true);
+                                string baseLine;
+                                while ((baseLine = basic.ReadLine()) != null)
+                                {
+                                    string[] baseSplit = baseLine.Split(',');
+
+                                    if (baseSplit[0].Contains(goalSplit[i]))
+                                    {
+                                        results += goalSplit[i] + "|";
+                                        if (checkShort.Checked == true)
+                                        {
+                                            results += baseSplit[1] + "|";
+                                        }
+                                        if (checkLong.Checked == true)
+                                        {
+                                            results += baseSplit[2];
+                                        }
+
+                                        finished.WriteLine(results);
+                                        results = "";
+                                        break;
+                                    }
+                                }
+                                finished.Flush();
+                                finished.Close();
+                                basic.Close();
+
+                            }
+                        }
+                        goal.Close();
+
+
+                    }
+
+
+                }
+
+
+
+
+
+            }
+
+        }
+    }
+}
